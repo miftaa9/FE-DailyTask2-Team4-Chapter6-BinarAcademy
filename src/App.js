d556b260b57652b264e0ed9e2ca04fecab21740e
miftaaaaaa <miftakhul.20015@mhs.unesa.ac.id>
2023-05-20 22:28:38 +0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 
 function ColorSchemesExample() {
   return (
@@ -23,53 +20,3 @@ function ColorSchemesExample() {
 }
 
 export default ColorSchemesExample;
-=======
-import NavDropdown from 'react-bootstrap/NavDropdown';
->>>>>>> 8c3249a76117f3578e55ebeafccd847d57a9f560
-
-function ColorSchemesExample() {
-  return (
-    <>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <br />
-      <Navbar bg="primary" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-
-      <br />
-      <Navbar bg="light" variant="light">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
-  );
-}
-
-<<<<<<< HEAD
-export default ColorSchemesExample;
-=======
-export default BasicExample;
->>>>>>> 5a9e961d3d7753d191fbb5f69e26eae552f6e7e5
->>>>>>> 8c3249a76117f3578e55ebeafccd847d57a9f560
