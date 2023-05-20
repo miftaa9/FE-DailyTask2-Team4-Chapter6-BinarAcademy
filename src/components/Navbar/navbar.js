6405833edece21e146a408cb44f7a0e172ec7cbc
endanghendayatna <a2.2000125@mhs.stmik-sumedang.ac.id>
2023-05-20 23:15:34 +0700

    terserah

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -7,7 +7,7 @@ function ColorSchemesExample() {
     <>
       <Navbar bg="primary" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">Logooooo</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
