18687be45705722dff95b3725ac867f4b3f0b953
raflimardhian <raflimardhian13@gamil.com>
2023-05-21 21:40:09 +0700

    anti error

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import './App.css';
 import ColorSchemesExample from './components/Navbar/navbar';
-import BasicExample from './components/Cards/cards';
+import BasicExample from './components/Cards/cards'
 import Example from './components/Modal/addproduct';
-<<<<<<< HEAD
 import Footer from './components/Footer/footer';
 import Carousell from './components/Carousel/carousel';
-=======
-import AboutUs from './components/AbousUs/about-us';
 import Login from './components/Login/login';
->>>>>>> b13d1e04bed65bd32f9bc5a4f3d9f21a9353cbac
+import Cards from './components/AbousUs/about-us';
 
 function App() {
   return (
     <div className="App">
-      {/* <Carousell/> */}
       <Login/>
+      <Carousell/>
       <ColorSchemesExample />
-      <BasicExample/>
       <div className='example'>
         <Example /> 
       </div>
-      {/* <KitchenSinkExample /> */}
-      <AboutUs/>
-      {/* <Footer/> */}
+      <BasicExample />
+      <Footer/>
     </div>
   );
 }
