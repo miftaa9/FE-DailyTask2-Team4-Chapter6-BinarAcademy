c2095d4b31a8fa1199ac8422ce3aac89d532356c
miftaaaaaa <miftakhul.20015@mhs.unesa.ac.id>
2023-05-21 20:23:34 +0700

    login page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import './App.css';
 import ColorSchemesExample from './components/Navbar/navbar';
 import BasicExample from './components/Cards/cards'
 import Example from './components/Modal/addproduct';
-<<<<<<< HEAD
 import NewExpense from './components/NewExpense/NewExpense';
 // import AboutUs from './components/AbousUs/about-us';
 import Amount from './components/AbousUs/pp'
-=======
-import Footer from './components/Navbar/footers';
-import Carousell from './components/Carousel/carousel';
->>>>>>> a23e7338044a1333749fa8b05498288c5e39d8b3
 
 function App() {
   return (
@@ -19,16 +14,11 @@ function App() {
       <div className='example'>
         <Example /> 
       </div>
-<<<<<<< HEAD
       <KitchenSinkExample />
       <NewExpense /> 
       {/* <AboutUs></AboutUs> */}
       <Amount></Amount>
       {/* <Footer/> */}
-=======
-      <BasicExample />
-      <Footer/>
->>>>>>> a23e7338044a1333749fa8b05498288c5e39d8b3
     </div>
   );
 }
