a23e7338044a1333749fa8b05498288c5e39d8b3
rizagithub <rizatul.20004@mhs.unesa.ac.id>
2023-05-21 18:51:16 +0700

    day 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
 import ColorSchemesExample from './components/Navbar/navbar';
-import KitchenSinkExample from './components/Cards/cards'
+import BasicExample from './components/Cards/cards'
 import Example from './components/Modal/addproduct';
-import NewExpense from './components/NewExpense/NewExpense';
+import Footer from './components/Navbar/footers';
+import Carousell from './components/Carousel/carousel';
 
 function App() {
   return (
     <div className="App">
+      <Carousell/>
       <ColorSchemesExample />
       <div className='example'>
-        <Example />
+        <Example /> 
       </div>
-      <KitchenSinkExample />
-      <NewExpense /> 
-      {/* <Footer/> */}
+      <BasicExample />
+      <Footer/>
     </div>
   );
 }
