b46b90534aa5a27b9f3238b545c2f0f7d0dc9fd0
rizagithub <rizatul.20004@mhs.unesa.ac.id>
2023-05-22 00:14:10 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@ import './App.css';
 import ColorSchemesExample from './components/Navbar/navbar';
 import BasicExample from './components/Cards/cards';
 import Example from './components/Modal/addproduct';
-<<<<<<< HEAD
 import Footer from './components/Footer/footer';
 import Carousell from './components/Carousel/carousel';
-=======
-import AboutUs from './components/AbousUs/about-us';
-import Login from './components/Login/login';
->>>>>>> b13d1e04bed65bd32f9bc5a4f3d9f21a9353cbac
 
 function App() {
   return (
     <div className="App">
-      {/* <Carousell/> */}
+      <Carousell/>
       <Login/>
       <ColorSchemesExample />
       <BasicExample/>
@@ -22,7 +17,6 @@ function App() {
       </div>
       {/* <KitchenSinkExample /> */}
       <AboutUs/>
-      {/* <Footer/> */}
     </div>
   );
 }
