c18053860e15c84f3212d112c16e74326fc7df8b
Huda <xhudan@yandex.com>
2023-05-22 16:49:44 +0700

    fix cards

--- src/components/Cards/cards.js
+++ src/components/Cards/cards.js
@@ -1,23 +1,9 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './card.css'
-<<<<<<< HEAD
 import Detail from '../Modal/detail';
-=======
-import Popup from '../Popup/popup';
-import React, { useState } from "react";
->>>>>>> dcc82abef14ff628e3179128543f1b1cf67569b6
 
 function BasicExample() {
-
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
-  const handleOpenPopup = () => {
-    setIsPopupOpen(true);
-  };
-  const handleClosePopup = () => {
-    setIsPopupOpen(false);
-  };
-
   return (
     <Card className='card' style={{ width: '18rem' }}>
       <div style={{ fontSize: '10px' }} className='kategori'>
@@ -32,25 +18,13 @@ function BasicExample() {
           <h6>Rp. 100.000</h6>
         </Card.Text>
         <div className='button'>
-<<<<<<< HEAD
         <Detail/>
         {/* <Button variant="success">Detail</Button> */}
-=======
-        <div>
-          {isPopupOpen ? (
-            <Popup onClose={handleClosePopup} />
-              ) : (
-            <Button variant="success" onClick={handleOpenPopup}>Detail</Button>
-            )}
-        </div>
->>>>>>> dcc82abef14ff628e3179128543f1b1cf67569b6
         <Button style={{margin:'7px'}} variant="warning">Update</Button>
         <Button variant="danger">Delete</Button>
         </div>
       </Card.Body>
     </Card>
-
-    
   );
 }
 
