59380d7d9bcba8bb148d178ee93dea9e92756496
Riza <113010774+rizagithub@users.noreply.github.com>
2023-05-22 14:19:35 +0700

    Update BootstrapCarousel.js

--- src/components/Carousel/BootstrapCarousel.js
+++ src/components/Carousel/BootstrapCarousel.js
@@ -6,7 +6,7 @@ import Carousel from 'react-bootstrap/Carousel';
 
 function BootstrapCarousel() {
   return (
-    <div>
+    <div style={{paddingTop:'70px'}}>
       <Carousel>
         <Carousel.Item>
         <img
